86d6cb027bc72f16feb2a8adaa800437d5750b50
EthanHaller <ttk4ey@virginia.edu>
2023-06-09 11:14:49 -0400

    added all

---
+++ routes/stripe.js
@@ -0,0 +1,64 @@
+var express = require("express")
+var router = express.Router()
+var Stripe = require("stripe")
+var stripe = Stripe(process.env.REACT_APP_STRIPE_SECRET)
+
+router.post("/", async (req, res, next) => {
+	const { items } = req.body
+
+	const lineItems = items.map((item) => {
+		return {
+			price_data: {
+				currency: "usd",
+				product_data: {
+					name: item.title,
+					description: item.description,
+					images: item.images,
+					metadata: {
+						itemId: item.id
+					}
+				},
+				unit_amount: item.price * 100,
+			},
+			quantity: 1,
+		}
+	})
+
+	const session = await stripe.checkout.sessions.create({
+		line_items: lineItems,
+		mode: "payment",
+		success_url:
+			"http://localhost:3000/order/success?session_id={CHECKOUT_SESSION_ID}",
+		cancel_url: "http://localhost:3000/cart",
+	})
+	res.send(session.url)
+})
+
+router.get("/order/success", async (req, res, next) => {
+    const session = await stripe.checkout.sessions.retrieve(
+        req.query.session_id
+    );
+    const lineItems = await stripe.checkout.sessions.listLineItems(
+        req.query.session_id
+    );
+
+    // Fetch the product data for each line item
+    const lineItemsWithMetadata = await Promise.all(
+        lineItems.data.map(async (lineItem) => {
+            const product = await stripe.products.retrieve(
+                lineItem.price.product
+            );
+            return {
+                ...lineItem,
+                metadata: product.metadata
+            };
+        })
+    );
+
+    res.send({
+        results: session.customer_details.name,
+        lineItems: lineItemsWithMetadata
+    });
+});
+
+module.exports = router
